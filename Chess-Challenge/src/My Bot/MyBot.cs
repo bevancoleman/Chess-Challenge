560ffa2c858cfd440d4cb978d5da94d1cb286dcb
Bevan Coleman <817786+bevancoleman@users.noreply.github.com>
2024-02-22 21:19:56 +1030

    Unfinished

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,69 +6,25 @@ using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
-    // See PieceType
-    // None, Pawn, Knight, Bishop, Rook, Queen, King
-    private int[] _pieceValue = { 0, 10, 30, 30, 50, 90, 900 };
+    private const int MaxDepth = 4;
 
     // First movies - rather than leave it to calculation, just take a popular one.
     // https://www.masterclass.com/articles/chess-101-what-are-the-best-opening-moves-in-chess-learn-5-tips-for-improving-your-chess-opening
     // White, Black, White, etc...
     private string[] _moveList = { "e2e4", "c7c5" };
 
-    
-    
-    private bool _playingWhite;
+    // See PieceType
+    // None, Pawn, Knight, Bishop, Rook, Queen, King
+    private int[] _pieceValue = { 0, 10, 30, 30, 50, 90, 900 };
     private bool _playingFromStart;
-    private TreeNode _treeRoot;
-    private ulong? _startZobristKey;
-    
-    private const int MaxDepth = 4;
+
+
+    private bool _playingWhite;
 
 
     private Dictionary<ulong, int> _scoreCache = new();
-
-    public class TreeNode : IEnumerable<TreeNode>
-    {
-        private readonly Dictionary<Move, TreeNode> _children = new();
-
-        public readonly Move Move; //ID
-        public readonly ulong ZobristKey;
-       
-        public TreeNode Parent { get; private set; }
-
-        public TreeNode(Move move, ulong zobristKey)
-        {
-            Move = move;
-            ZobristKey = zobristKey;
-        }
-
-        public TreeNode GetChild(Move move)
-        {
-            return _children[move];
-        }
-
-        public void Add(TreeNode item)
-        {
-            if (item.Parent != null)
-            {
-                item.Parent._children.Remove(item.Move);
-            }
-            item.Parent = this;
-            _children.Add(item.Move, item);
-        }
-
-        public IEnumerator<TreeNode> GetEnumerator()
-        {
-            return _children.Values.GetEnumerator();
-        }
-        
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return _children.Values.GetEnumerator();
-        }
-        
-        public int Count => _children.Count;
-    }
+    private ulong? _startZobristKey;
+    private TreeNode _treeRoot;
 
     public Move Think(Board board, Timer timer)
     {
@@ -79,7 +35,7 @@ public class MyBot : IChessBot
             _playingFromStart = board.ZobristKey == 13227872743731781434; // ZobristKey from a Fresh Board. Got this inline because it's more compact to do inline.
             _playingWhite = board.IsWhiteToMove;
         }
-        
+
         // TODO remove this, this is just for testing to make sure nothing is going wrong. It wastes tokens space though.
         if (_playingWhite != board.IsWhiteToMove)
             throw new InvalidOperationException("Somehow changed side, probably reused ");
@@ -97,7 +53,7 @@ public class MyBot : IChessBot
                     return new Move(_moveList[1], board);
             }
         }
-        
+
         // Start by making sure we have our root
         if (_treeRoot == null)
         {
@@ -109,7 +65,7 @@ public class MyBot : IChessBot
             // Update tree to point to opposition move... we dont' care about paths not taken or history (we can get that from the board anyway)
             _treeRoot = _treeRoot.GetChild(board.GameMoveHistory.Last());
         }
-        
+
         // Update Tree, this should reuse the tree and add depth
         GenerateMoves(board, _treeRoot, MaxDepth, _playingWhite);
 
@@ -131,7 +87,7 @@ public class MyBot : IChessBot
 
         // Update TreeRoot to move taken;
         _treeRoot = _treeRoot.GetChild(pickedMove);
-        
+
         // Return move
         return pickedMove;
     }
@@ -141,20 +97,20 @@ public class MyBot : IChessBot
         // Hit max depth, abort
         if (depth == 0)
             return;
-        
+
         // No Children (yet?)
         if (parentNode.Count == 0)
         {
             var moves = board.GetLegalMoves();
             var maxChildScore = int.MinValue;
-            
+
             foreach (var move in moves)
             {
                 // Try Move
                 board.MakeMove(move);
-                
-                var checkmate = board.IsInCheckmate();                
-                
+
+                var checkmate = board.IsInCheckmate();
+
                 // Calculate score if we don't already know it.
                 int moveScore;
                 if (!_scoreCache.ContainsKey(board.ZobristKey))
@@ -193,7 +149,7 @@ public class MyBot : IChessBot
     /// <returns></returns>
     public int Minimax(TreeNode node, int depth, bool maximizingPlayer)
     {
-        if (depth == 0 || node.Count == 0)  // At max depth, or hit a leaf
+        if (depth == 0 || node.Count == 0) // At max depth, or hit a leaf
         {
             // TODO: This assumes each leaf-node has a score generated.
             return _scoreCache[node.ZobristKey];
@@ -212,6 +168,7 @@ public class MyBot : IChessBot
             foreach (var child in node)
                 value = Math.Min(value, Minimax(child, depth - 1, true));
         }
+
         return value;
     }
 
@@ -228,9 +185,53 @@ public class MyBot : IChessBot
         foreach (var piece in board.GetAllPieceLists())
         {
             // Calc value, make sure negative value for other side pieces.
-            value += _pieceValue[(int)(piece.TypeOfPieceInList)] * piece.Count * (piece.IsWhitePieceList == playingWhite ? 1: -1);
+            value += _pieceValue[(int)(piece.TypeOfPieceInList)] * piece.Count * (piece.IsWhitePieceList == playingWhite ? 1 : -1);
         }
 
         return value;
+    }
+
+    public class TreeNode : IEnumerable<TreeNode>
+    {
+        private readonly Dictionary<Move, TreeNode> _children = new();
+
+        public readonly Move Move; //ID
+        public readonly ulong ZobristKey;
+
+        public TreeNode(Move move, ulong zobristKey)
+        {
+            Move = move;
+            ZobristKey = zobristKey;
+        }
+
+        public TreeNode Parent { get; private set; }
+
+        public int Count => _children.Count;
+
+        public IEnumerator<TreeNode> GetEnumerator()
+        {
+            return _children.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _children.Values.GetEnumerator();
+        }
+
+        public TreeNode GetChild(Move move)
+        {
+            return _children[move];
+        }
+
+        public void Add(TreeNode item)
+        {
+            if (item.Parent != null)
+            {
+                item.Parent._children.Remove(item.Move);
+            }
+
+            item.Parent = this;
+            _children.Add(item.Move, item);
+        }
     }
 }
--- MyBotTests/MyBotCheckMateTests.cs
+++ MyBotTests/MyBotCheckMateTests.cs
@@ -4,38 +4,54 @@ using Timer = ChessChallenge.API.Timer;
 [TestFixture]
 public class MyBotCheckMateTests
 {
-    private const string FenStartingBoard = "8/8/3qk3/8/8/3KR3/8/8 w - - 0 1";  //https://www.chess.com/analysis?tab=analysis
-    private const int GameTimeMs = 60*1000;
-    
-    private Board _board;
-    private IChessBot _chessBotWhite;
-    private Timer _timer;
-
     /// <summary>
     /// Setup before each test, this makes sure to reset to a fresh playing field before each test.
     /// </summary>
     [SetUp]
     public void SetupBeforeEachTest()
     {
-        _board = Board.CreateBoardFromFEN(FenStartingBoard);
-        _chessBotWhite = new MyBot();
+        _chessBot = new MyBot();
         _timer = new Timer(GameTimeMs, GameTimeMs, GameTimeMs);
     }
-    
 
-   
+    private const string FenCheckMateBlack = "k7/8/8/8/8/8/R7/1R6 w - - 0 1"; //https://www.chess.com/analysis?tab=analysis
+    private const string FenCheckMateWhite = ""; //https://www.chess.com/analysis?tab=analysis
+    private const int GameTimeMs = 60 * 1000;
+
+    private IChessBot _chessBot;
+    private Timer _timer;
+
+
     /// <summary>
-    /// Test to check board value generation.
+    /// Test scoring if white and black is check-mated
     /// </summary>
     [Test]
-    public void WhiteFirstMove_InCheckMate_ValueCheck()
+    public void WhiteMove_InCheckMate_ValueCheck()
     {
-        // Expect
-        const int expectedValue = 900; // Board is in Checkmate for white
-        
+        // Setup
+        const int expectedValue = 900;
+        var board = Board.CreateBoardFromFEN(FenCheckMateBlack);
+
         // Test
-        var resultValue = ((MyBot)_chessBotWhite).CalculateBoardValue(_board, true);
-        
+        var resultValue = ((MyBot)_chessBot).CalculateBoardValue(board, true);
+
+        // Assert
+        Assert.That(resultValue, Is.EqualTo(expectedValue));
+    }
+
+    /// <summary>
+    /// Test scoring if black and white is check-mated
+    /// </summary>
+    [Test]
+    public void BackMove_InCheckMate_ValueCheck()
+    {
+        // Setup
+        const int expectedValue = 900;
+        var board = Board.CreateBoardFromFEN(FenCheckMateWhite);
+
+        // Test
+        var resultValue = ((MyBot)_chessBot).CalculateBoardValue(board, false);
+
         // Assert
         Assert.That(resultValue, Is.EqualTo(expectedValue));
     }
